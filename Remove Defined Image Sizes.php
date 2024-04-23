60f1c1639919dcd6fea0cf65eb54a13686e3267a
Elvinas <elvinasblcns@gmail.com>
2024-04-24 01:15:15 +0300

    Update Remove Defined Image Sizes.php

--- Remove Defined Image Sizes.php
+++ Remove Defined Image Sizes.php
@@ -1,4 +1,5 @@
 <?php
+require 'wp-config.php';
 
 add_filter( 'intermediate_image_sizes', 'remove_default_img_sizes', 10, 1);
 
@@ -13,3 +14,31 @@ function remove_default_img_sizes( $sizes ) {
 
   return $sizes;
 }
+
+
+// list all image sizes
+function _get_all_image_sizes() {
+    global $_wp_additional_image_sizes;
+
+    $default_image_sizes = get_intermediate_image_sizes();
+
+    foreach ( $default_image_sizes as $size ) {
+        $image_sizes[ $size ][ 'width' ] = intval( get_option( "{$size}_size_w" ) );
+        $image_sizes[ $size ][ 'height' ] = intval( get_option( "{$size}_size_h" ) );
+        $image_sizes[ $size ][ 'crop' ] = get_option( "{$size}_crop" ) ? get_option( "{$size}_crop" ) : false;
+    }
+
+    if ( isset( $_wp_additional_image_sizes ) && count( $_wp_additional_image_sizes ) ) {
+        $image_sizes = array_merge( $image_sizes, $_wp_additional_image_sizes );
+    }
+
+    return $image_sizes;
+}
+
+// Get available image sizes
+$sizes = get_intermediate_image_sizes();
+
+// Output the list of available image sizes
+echo '<pre>';
+print_r($sizes);
+echo '</pre>';
