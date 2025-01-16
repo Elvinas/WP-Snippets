94b55c71460ed0c284204f1c064bd0ac36cef82a
Elvinas <elvinasblcns@gmail.com>
2025-01-16 22:35:13 +0200

    Update WooCommerce useful.php

--- WooCommerce useful.php
+++ WooCommerce useful.php
@@ -1,4 +1,9 @@
 <?php
+
+is_user_logged_in()
+get_current_user_id
+$order->set_user_id($user_id)
+	
 // Before checkout form
 do_action( 'woocommerce_before_checkout_form', $checkout );
 
@@ -68,11 +73,37 @@ WC()->customer->get_shipping_address_1()
 WC()->customer->get_shipping_address_2()
 WC()->customer->get_shipping_address_2()
 WC()->customer->get_shipping_postcode()
+ $order->set_billing_first_name($firstname);
+        $order->set_billing_last_name($lastname);
+        $order->set_billing_email($email);
+        $order->set_billing_phone($phone_number);
+        $order->set_billing_address_1($street);
+        $order->set_billing_address_2($house_number . ' ' . $apartment_number);
+        $order->set_billing_city($city);
+        $order->set_billing_postcode($postal_code);
+        $order->set_billing_country($country);
+        $order->set_billing_state($area);
 
+$order->set_shipping_first_name($firstname);
+    $order->set_shipping_last_name($lastname);
+    $order->set_shipping_address_1($street);
+    $order->set_shipping_address_2($house_number . ' ' . $apartment_number);
+    $order->set_shipping_city($city);
+    $order->set_shipping_postcode($postal_code);
+    $order->set_shipping_country($country);
+    $order->set_shipping_state($area);
+$order->calculate_totals();
+    $order->calculate_shipping();
+ $order->update_status('pending');
+$order->save();
+ $order->get_id();
+$shipping_method = WC()->session->get('chosen_shipping_methods');
+$order->set_shipping_method(array($shipping_method[0]));
 get_template_directory_uri() 
 
+	wc_get_product
+	 $order->add_product($product, $quantity);
 WC()->cart->is_empty()
-WC()->cart->get_cart()
 WC()->cart->get_cart()
 $product->get_price_excluding_tax()
 $product->get_price_including_tax()
@@ -87,3 +118,7 @@ WC()->cart->get_shipping_total()
 WC()->cart->get_total_tax()
 WC()->cart->get_total('edit')
 wp_nonce_field( 'woocommerce-checkout', 'woocommerce_checkout_nonce' )
+
+sanitize_text_field
+sanitize_email
+wc_create_order
